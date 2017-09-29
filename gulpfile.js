b06a684f23a93f7fccb4c6cb1d27a9525a6b3b32
Mikolaj Dobrucki <mikolaj.dobrucki@gmail.com>
2017-09-29 17:17:16 +0200

    add material foundation to sass

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var $    = require('gulp-load-plugins')();
 var sassPaths = [
   'bower_components/normalize.scss/sass',
   'bower_components/foundation-sites/scss',
-  'bower_components/motion-ui/src'
+  'bower_components/motion-ui/src',
+  'node_modules/materialfoundation/scss/'
 ];
 
 gulp.task('sass', function() {
